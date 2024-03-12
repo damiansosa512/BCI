33a7cc333c84eb40ee13080c57aa2df271cf128e
roberto.sosa <roberto.sosa@globallogic.com>
2024-03-12 10:24:33 -0300

    Import java.validation

--- src/main/java/com/bci/cl/demo/dto/UserDto.java
+++ src/main/java/com/bci/cl/demo/dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
